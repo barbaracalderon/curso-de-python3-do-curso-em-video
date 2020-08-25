380722f5b22054748172033aa87052aa0209afad
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-25 10:47:19 -0300

    Add files via upload

---
+++ desafio005.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número: '))
+suce = numero+1
+ante = numero-1
+print('O sucessor desse número é {} e o antecessor é {}.'.format(suce, ante))
---
+++ desafio006.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+dobro = numero*2
+triplo = numero*3
+raiz = numero**(1/2)
+print('O dobro desse número é {}, seu triplo é {} e a raiz quadrada dele é {:.2f}.'.format(dobro, triplo, raiz))
