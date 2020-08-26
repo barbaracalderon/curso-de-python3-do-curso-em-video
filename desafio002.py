e08abcc1db41ec32b0a1dff806c688d3d87afdf1
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-26 08:52:34 -0300

    Comment added

--- desafio002.py
+++ desafio002.py
@@ -1,3 +1,6 @@
+# Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa
+# e mostre uma mensagem com a data formatada.
+
 dia = input('Dia = ')
 mes = input('Mes = ')
 ano = input('Ano = ')
