c85218212bd65f87e7180f0555cf0b70e3d1fffd
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-26 08:51:03 -0300

    Comment added

--- desafio001.py
+++ desafio001.py
@@ -1,2 +1,6 @@
+#Crie um script Python que leia o nome de uma pessoa
+#e mostre uma mensagem de boas-vindas de acordo com o
+#valor digitado.
+
 nome = input('Qual é o seu nome? ')
 print('Olá,', nome, '! Prazer em te conhecer!')
