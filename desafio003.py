ca54c77e7b3f1fa18447790bf991ab93337a14ea
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-26 08:53:27 -0300

    Comment added

--- desafio003.py
+++ desafio003.py
@@ -1,3 +1,6 @@
+# Crie um script Python que leia dois números e tente mostrar
+# a soma entre eles.
+
 numero_1 = int(input('Digite o primeiro número '))
 numero_2 = int(input('Digite o segundo número '))
 print('A soma entre eles é', numero_1+numero_2)
