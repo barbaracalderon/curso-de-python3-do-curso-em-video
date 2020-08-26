0310f4bc1e828be55bde789ba7b8b0c91b4adb1a
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-26 08:57:35 -0300

    Add files via upload

---
+++ desafio007.py
@@ -0,0 +1,4 @@
+nota_1 = float(input('Digite sua primeira nota: '))
+nota_2 = float(input('Digite sua segunda nota: '))
+media = (nota_1 + nota_2)/2
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(nota_1, nota_2, media))
