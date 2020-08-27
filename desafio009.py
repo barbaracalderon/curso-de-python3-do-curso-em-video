1ca8682dc3a94de609d8f75fa55122d8ed36f68e
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-08-27 08:54:58 -0300

    Add files via upload

---
+++ desafio009.py
@@ -0,0 +1,27 @@
+# Faça um programa que leia um número inteiro qualquer
+# e mostre na tela a sua tabuada.
+
+numero = int(input('Digite um número: '))
+zero = numero*0
+um = numero*1
+dois = numero*2
+tres = numero*3
+quatro = numero*4
+cinco = numero*5
+seis = numero*6
+sete = numero*7
+oito = numero*8
+nove = numero*9
+dez = numero*10
+print('TABUADA DO {}:'.format(numero))
+print('{} x 0 = {}'.format(numero, zero))
+print('{} x 1 = {}'.format(numero, um))
+print('{} x 2 = {}'.format(numero, dois))
+print('{} x 3 = {}'.format(numero, tres))
+print('{} x 4 = {}'.format(numero, quatro))
+print('{} x 5 = {}'.format(numero, cinco))
+print('{} x 6 = {}'.format(numero, seis))
+print('{} x 7 = {}'.format(numero, sete))
+print('{} x 8 = {}'.format(numero, oito))
+print('{} x 9 = {}'.format(numero, nove))
+print('{} x 10 = {}'.format(numero, dez))
