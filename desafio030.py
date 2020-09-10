9ca245c5b7c79399304e4eafbaa49bf27e316992
barbaracalderon <69126514+barbaracalderon@users.noreply.github.com>
2020-09-10 16:32:42 -0300

    Add files via upload

---
+++ desafio030.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um número inteiro
+# e mostre na tela se ele é PAR ou ÍMPAR.
+
+numero = int(input('Digite um número inteiro: '))
+if numero % 2 == 0:
+    print('Esse número é PAR.')
+else:
+    print('Esse número é ÍMPAR.')
