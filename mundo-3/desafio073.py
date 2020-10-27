fc4b59d539a491d8c5a8ddcdf66f19c715d3284a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-27 09:29:13 -0300

    Inserted exception lines 17, 18, 27

--- mundo-3/desafio073.py
+++ mundo-3/desafio073.py
@@ -10,9 +10,12 @@ brasileirao = ('Internacional', 'Flamengo', 'Atlético-MG', 'Fluminense', 'São 
                'Sport Recife', 'Corinthians', 'Bahia', 'Bragantino-SP', 'Botafogo',
                'Vasco da Gama', 'Athletico-PR', 'Coritiba', 'Goiás')
 posicao = 0
+mensagem = ''
 for c in brasileirao:
     if c == 'Chapecoense':
         posicao = c + 1
+    if posicao == 0:
+        mensagem = ', este time não está na série A!'
 print('================= [BRASILEIRÃO - SÉRIE A] ==================')
 print('De acordo com a Tabela de Classificação (do dia 27/10/2020):')
 print(f'5 PRIMEIROS COLOCADOS:\n{brasileirao[:5]}')
@@ -21,4 +24,4 @@ print(f'OS ÚLTIMOS 4 COLOCADOS (LANTERNINHAS):\n{brasileirao[-4:]}')
 print('------------------------------------------------------------')
 print(f'LISTA EM ORDEM ALFABÉTICA:\n{sorted(brasileirao)}')
 print('------------------------------------------------------------')
-print(f'POSIÇÃO DA CHAPECOENSE NA TABELA: {posicao}')
+print(f'POSIÇÃO DA CHAPECOENSE NA TABELA: {posicao}{mensagem}')
