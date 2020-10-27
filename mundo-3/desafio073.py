2a642d86debba2c558f799c80233e2b678a67b8d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-27 09:13:32 -0300

    Created soccer challenge

---
+++ mundo-3/desafio073.py
@@ -0,0 +1,24 @@
+# Crie uma tupla preenchida com os 20 primeiros colocados da Tabela
+# do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois, mostre:
+# a) Apenas os 5 primeiros colocados;
+# b) Os últimos 4 colocados da tabela;
+# c) Uma lista com os times em ordem alfabética;
+# e) Em que posição na tabela está o time da Chapecoense.
+# Obs.: Hoje é 27 de outubro de 2020.
+brasileirao = ('Internacional', 'Flamengo', 'Atlético-MG', 'Fluminense', 'São Paulo',
+               'Santos', 'Palmeiras', 'Fortaleza', 'Grêmio', 'Ceará SC', 'Atlético-GO',
+               'Sport Recife', 'Corinthians', 'Bahia', 'Bragantino-SP', 'Botafogo',
+               'Vasco da Gama', 'Athletico-PR', 'Coritiba', 'Goiás')
+posicao = 0
+for c in brasileirao:
+    if c == 'Chapecoense':
+        posicao = c + 1
+print('================= [BRASILEIRÃO - SÉRIE A] ==================')
+print('De acordo com a Tabela de Classificação (do dia 27/10/2020):')
+print(f'5 PRIMEIROS COLOCADOS:\n{brasileirao[:5]}')
+print('------------------------------------------------------------')
+print(f'OS ÚLTIMOS 4 COLOCADOS (LANTERNINHAS):\n{brasileirao[-4:]}')
+print('------------------------------------------------------------')
+print(f'LISTA EM ORDEM ALFABÉTICA:\n{sorted(brasileirao)}')
+print('------------------------------------------------------------')
+print(f'POSIÇÃO DA CHAPECOENSE NA TABELA: {posicao}')
