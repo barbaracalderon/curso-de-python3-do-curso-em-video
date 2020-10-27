1e7c721a8d34373189132a8c5ad4db140f0cd52d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-26 21:25:02 -0300

    Simplified line 10

--- mundo-3/desafio072.py
+++ mundo-3/desafio072.py
@@ -7,11 +7,10 @@ while True:
     valor = int(input('Digite um valor entre 0 e 20: '))
     while valor < 0 or valor > 20:
         valor = int(input('Valor inválido. Digite um valor entre 0 e 20: '))
-    tupla = (0, 'zero', 1, 'um', 2, 'dois', 3, 'três', 4, 'quatro', 5, 'cinco', 6, 'seis', 7, 'sete', 8, 'oito', 9, 'nove', 10, 'dez', 11, 'onze', 12, 'doze', 13, 'treze', 14, 'quatorze', 15, 'quinze', 16, 'dezesseis', 17, 'dezessete', 18, 'dezoito', 19, 'dezenove', 20, 'vinte')
-    for c in tupla:
-        if c == valor:
-            posicao = valor * 2 + 1
-            print(f'Você digitou {valor} e seu nome por extenso é {tupla[posicao]}.')
+    tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',
+             'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
+             'dezessete', 'dezoito', 'dezenove', 'vinte')
+    print(f'Você digitou {valor} e seu nome por extenso é {tupla[valor]}.')
     continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
     if continuar == 'N':
         print('Saindo do programa. Volte sempre!')
