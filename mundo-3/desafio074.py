c3e3b84f349d7fea6229c3be77ea03921668dfdc
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-28 16:25:31 -0300

    Challenge finished

--- mundo-3/desafio074.py
+++ mundo-3/desafio074.py
@@ -1,3 +1,9 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar
 # em uma tupla. Depois disso, mostre a listagem de números gerados e
 # também indique o menor e o maior valor que estão na tupla.
+from random import randint
+print('================== [NÚMEROS ALEATÓRIOS] ===================')
+tupla = (randint(0,100), randint(0,100), randint(0,100), randint(0,100), randint(0,100))
+print(f'Números gerados aleatoriamente: {tupla}')
+print(f'Maior valor: {sorted(tupla)[-1]}')
+print(f'Menor valor: {sorted(tupla)[0]}')
