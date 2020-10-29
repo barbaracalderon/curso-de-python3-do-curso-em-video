8999f355b2f6d41c7db41e2177a2acde3b6e70f5
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-29 00:03:11 -0300

    First half is done

---
+++ mundo-3/desafio075.py
@@ -0,0 +1,11 @@
+# Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+# No final, mostre:
+# a) Quantas vezes apareceu o valor 9;
+# b) Em que posição foi digitado o primeiro valor 3;
+# c) Quais foram os números pares.
+a = int(input('Digite o 1ª valor: '))
+b = int(input('Digite o 2ª valor: '))
+c = int(input('Digite o 3ª valor: '))
+d = int(input('Digite o 4ª valor: '))
+tupla = (a, b, c, d)
+print(f'Os valores que você forneceu foram: {tupla}')
