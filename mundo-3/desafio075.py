54d300971d44d23d9bf70ceaa932b40b995480bc
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-29 00:56:09 -0300

    erased the number 9 counter

--- mundo-3/desafio075.py
+++ mundo-3/desafio075.py
@@ -3,24 +3,21 @@
 # a) Quantas vezes apareceu o valor 9;
 # b) Em que posição foi digitado o primeiro valor 3;
 # c) Quais foram os números pares.
-print('=============== [04 NÚMEROS] ===============')
-a = int(input('Digite o 1ª valor: '))
-b = int(input('Digite o 2ª valor: '))
-c = int(input('Digite o 3ª valor: '))
-d = int(input('Digite o 4ª valor: '))
-tupla = (a, b, c, d)
-print(f'Os valores que você forneceu foram: {tupla}')
-cont_nove = cont_posicao = 0
+print('================== [04 NÚMEROS] ==================')
+tupla = (int(input('Digite o 1ª valor: ')),
+         int(input('Digite o 2ª valor: ')),
+         int(input('Digite o 3ª valor: ')),
+         int(input('Digite o 4ª valor: ')))
+cont_posicao = posicao_tres = 0
 pares = []
 for c in tupla:
     cont_posicao += 1
-    if c == 9:
-        cont_nove += 1
-    elif c == 3:
+    if c == 3:
         posicao_tres = cont_posicao
     elif c % 2 == 0:
         pares.append(c)
-print('-------------------------------------------')
-print(f'O número 9 apareceu {cont_nove} vezes')
+print('-------------------------------------------------')
+print(f'Os valores que você forneceu foram: {tupla}')
+print(f'O número 9 apareceu {tupla.count(9)} vezes')
 print(f'O número 3 apareceu primeiro na {posicao_tres}ª posição')
 print(f'Os números pares são: {pares}')
