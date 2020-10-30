8afd731cdb08f05f450e91198efeffd16c29415a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-10-30 11:15:57 -0300

    Challenge completed

--- mundo-3/desafio077.py
+++ mundo-3/desafio077.py
@@ -1,2 +1,9 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+tupla = ('cantar', 'brincar', 'cozinhar', 'estudar', 'trabalhar',
+         'aprender', 'oceano', 'passeio', 'ferias', 'aprendizado')
+for palavra in tupla:
+    print(f'\nNa palavra {palavra.upper()} temos as vogais ', end='')
+    for n in palavra:
+        if n in 'aeiou':
+            print(n.upper(), end='')
