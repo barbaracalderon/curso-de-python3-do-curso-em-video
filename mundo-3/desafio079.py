7955bddf7a462039276e6e96dcf50757f2a5ce95
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-01 14:19:49 -0300

    Almost there, needs to sort results

--- mundo-3/desafio079.py
+++ mundo-3/desafio079.py
@@ -1,3 +1,17 @@
 # Crie um programa onde o usuário possa digitar vários valores numéricos e
 # cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado.
 # No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+lista = []
+while True:
+    valor = int(input('Digite um valor: '))
+    if valor in lista:
+        pass
+    elif valor not in lista:
+        lista.append(valor)
+    continuar = str(input('Continuar? [S/N]: ')).upper().strip()
+    while continuar not in 'SN':
+        print('Valor inválido. Tente novamente.')
+        continuar = str(input('Continuar? [S/N]: ')).upper().strip()
+    if continuar == 'N':
+        break
+print(f'Os valores digitados, em ordem crescente, é {lista.sort(key=None, reverse=False)}')
