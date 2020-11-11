407b500844f33b87a44759835e67baaf6df65f34
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-11 15:07:45 -0300

    Sorted list without sort method

--- mundo-3/desafio080.py
+++ mundo-3/desafio080.py
@@ -1,3 +1,17 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e
 # cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()).
 # No final, mostre a lista ordenada na tela.
+lista = []
+for c in range (0, 5):
+    a = int(input('Digite um valor: '))
+    if c == 0 or a > lista[-1]:
+        lista.append(a)
+    else:
+        posicao = 0
+        while posicao < len(lista):
+            if a <= lista[posicao]:
+                lista.insert(posicao, a)
+                break
+            posicao += 1
+print('============================================')
+print(f'A lista ordenada é {lista}')
