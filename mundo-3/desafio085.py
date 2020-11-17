6f0172995dffa0c6ad460f948bd5a12c66bfa8d0
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-17 01:40:35 -0300

    Instructions inserted

---
+++ mundo-3/desafio085.py
@@ -0,0 +1,3 @@
+# Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em
+# uma lista única que mantenha separado os valores pares e ímpares. No final, mostre os
+# valores pares e ímpares em ordem crescente.
