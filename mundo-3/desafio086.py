b81c5a15f1d9e9ac0f4b210760a66d310ab53e30
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-20 18:30:33 -0300

    Raw idea

--- mundo-3/desafio086.py
+++ mundo-3/desafio086.py
@@ -1,2 +1,26 @@
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores
 # lidos pelo teclado. No final, mostre a matriz na tela com a formatação correta.
+print('=============== [MATRIZ 3x3] ===============')
+print('Vamos começar inserindo os dados para a sua matriz.')
+linha1 = []
+linha2 = []
+linha3 = []
+matriz = []
+cont = 1
+while cont < 10:
+    while cont < 4:
+        valor = str(input(f'Digite o {cont}ª valor: '))
+        linha1.append(valor)
+        cont += 1
+    while cont < 7:
+        valor = str(input(f'Digite o {cont}ª valor: '))
+        linha2.append(valor)
+        cont += 1
+    while cont < 10:
+        valor = str(input(f'Digite o {cont}ª valor: '))
+        linha3.append(valor)
+        cont += 1
+    matriz.append(linha1)
+    matriz.append(linha2)
+    matriz.append(linha3)
+print(matriz)
