ca6530191e49180c04601203a8acb0d3e6d83397
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-21 12:17:49 -0300

    Matrix 3x3

--- mundo-3/desafio086.py
+++ mundo-3/desafio086.py
@@ -9,18 +9,39 @@ matriz = []
 cont = 1
 while cont < 10:
     while cont < 4:
-        valor = str(input(f'Digite o {cont}ª valor: '))
+        valor = int(input(f'Digite o {cont}ª valor: '))
         linha1.append(valor)
         cont += 1
     while cont < 7:
-        valor = str(input(f'Digite o {cont}ª valor: '))
+        valor = int(input(f'Digite o {cont}ª valor: '))
         linha2.append(valor)
         cont += 1
     while cont < 10:
-        valor = str(input(f'Digite o {cont}ª valor: '))
+        valor = int(input(f'Digite o {cont}ª valor: '))
         linha3.append(valor)
         cont += 1
     matriz.append(linha1)
     matriz.append(linha2)
     matriz.append(linha3)
-print(matriz)
+print('===========================================')
+for e in matriz[0]:
+    print(f'[{e:^5}]', end=' ')
+print()
+for f in matriz[1]:
+    print(f'[{f:^5}]', end=' ')
+print()
+for g in matriz[2]:
+    print(f'[{g:^5}]', end=' ')
+
+'''
+Uma outra forma de realizar esse exercício, mostrada pelo prof. Guanabara, é a seguinte:
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+    matriz[l][c] = int(input(f'Digite um valor para [{l}], [{c}]: '))
+print('=-' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5]', end='')
+    print()
+'''
