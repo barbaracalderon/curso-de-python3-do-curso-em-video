367db3e286b48d88506d05c94f0a129761645af6
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-24 00:33:43 -0300

    Instructions inserted

---
+++ mundo-3/desafio094.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um
+# dicionário e todos os dicionários em uma lista. No final, mostre:
+# a) Quantas pessoas foram cadastradas;
+# b) A média de idade;
+# c) Uma lista com as mulheres;
+# d) Uma lista de pessoas com idade acima da média.
