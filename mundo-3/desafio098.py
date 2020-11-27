f5e9fc946754ad29ac6d8a016c6126f81b7a733a
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-27 13:33:20 -0300

    Instructions inserted

---
+++ mundo-3/desafio098.py
@@ -0,0 +1,5 @@
+# Faça um programa que tenha a função chamada contador(), que receba três parâmetros:
+# início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
+# a) de 1 até 10, de 1 em 1;
+# b) de 10 até 0, de 2 em 2;
+# c) uma contagem personalizada.
