4b79b2451328c2e47a524109befb2f6878f6d87d
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-27 12:48:54 -0300

    Custom made message

--- mundo-3/desafio097.py
+++ mundo-3/desafio097.py
@@ -1,2 +1,11 @@
 # Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer
 # como parâmetro e mostre a mensagem com tamanho adaptável.
+def escreva(texto):
+    print(f'~' * (len(texto) + 4))
+    print(f'  {texto}')
+    print(f'~' * (len(texto) + 4))
+
+
+# Programa Principal
+frase = str(input('Digite sua frase: '))
+escreva(frase)
