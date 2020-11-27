8d30cece4ca4c95f812d3e11ac0a06fad861ac66
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-27 01:45:05 -0300

    Soccer players

--- mundo-3/desafio095.py
+++ mundo-3/desafio095.py
@@ -4,22 +4,23 @@ print('======= [PLACAR DO JOGADOR] =========')
 dados = {}
 jogadores = []
 gols = []
+partidas = []
 soma = 0
 while True:
     dados.clear()
     dados["Jogador"] = str(input('Nome do jogador: '))
-    dados["Partidas"] = int(input('Número de partidas jogadas: '))
-    for g in range(0, dados["Partidas"]):
-        gol = int(input(f'Gols na partida {g+1}: '))
-        gols.append(gol)
-        dados["Gols"] = gols[:]
-    dados["Total de Gols"] = sum(gols)
+    total = int(input('Número de partidas jogadas: '))
+    partidas.clear()
+    for g in range(0, total):
+        partidas.append(int(input(f'Gols na partida {g+1}: ')))
+    dados["Gols"] = partidas[:]
+    dados["Total de Gols"] = sum(partidas)
+    jogadores.append(dados.copy())
     while True:
         continuar = str(input('Deseja continuar? [S/N]: ')).upper()[0]
         if continuar in 'SN':
             break
         print('Inválido. Digite S ou N.')
-    jogadores.append(dados.copy())
     if continuar == 'N':
         break
 print('=========== RELATÓRIO ========================================')
@@ -36,4 +37,15 @@ for k, v in enumerate(jogadores):
     for d in v.values():
         print(f'{str(d):<14}', end='')
     print()
-print()
+print('=============================================================')
+while True:
+    codigo = int(input('Mostrar dados de qual jogador? Digite seu código (999 para encerrar): '))
+    if codigo == 999:
+        break
+    if codigo >= len(jogadores):
+        print(f'Inválido! Não existe jogador com este código {codigo}.')
+    else:
+        print(f'<< DADOS DO JOGADOR {jogadores[codigo]["Jogador"].upper()} >> ')
+        for i, c in enumerate(jogadores[codigo]["Gols"]):
+            print(f' -> No jogo {i+1} fez {g} gols.')
+print('<< SISTEMA ENCERRADO >>')
