2a16ad9d949eb31f0aefceca42b9bc8a873c0ba8
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-28 18:52:54 -0300

    Number factorial

---
+++ mundo-3/desafio102.py
@@ -0,0 +1,30 @@
+# Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique
+# o número a calcular; e outro chamado show, que será um valor lógido (opcional) indicando se será mostrado
+# ou não na tela o processo de cálculo do fatorial.
+def fatorial(num=1, show=0):
+    f = 1
+    for c in range(num, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(f' = {f}\n', end='')
+        f *= c
+    return f
+
+
+# Programa Principal
+num = int(input('Digite um número: '))
+while True:
+    show = input('Mostrar a conta? [S/N]: ').upper()[0]
+    if show == 'S':
+        show = True
+        break
+    elif show == 'N':
+        show = False
+        break
+    else:
+        print('Código inválido. Digite "True" ou "False".')
+fatorial(num, show)
+print(f'O fatorial de {num} é {fatorial(num)}')
