91a0e89f86862bb20946332cd9caee145eb0149e
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-28 19:00:33 -0300

    Instructions inserted

---
+++ mundo-3/desafio103.py
@@ -0,0 +1,3 @@
+# Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais:
+# o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha
+# do jogador, mesmo que algum dado não tenha sido informado corretamente.
