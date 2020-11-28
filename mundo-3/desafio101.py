c0cd26274c52eedebb0c19194b5eef2ee0dce820
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-28 11:34:08 -0300

    Instructions inserted

---
+++ mundo-3/desafio101.py
@@ -0,0 +1,2 @@
+# Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma
+# pessoa, retornando um valor literal indicando se uma pessoa tem o voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
