17c6131fb73eff7598ab61ca96e65f1546029c28
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-29 14:38:22 -0300

    Data on a soccer player (with def)

--- mundo-3/desafio103.py
+++ mundo-3/desafio103.py
@@ -1,3 +1,18 @@
 # Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais:
 # o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha
 # do jogador, mesmo que algum dado não tenha sido informado corretamente.
+def ficha(n='<desconhecido>', g=0):
+    print(f'O jogador {n} fez {g} gols.')
+
+
+# Programa Principal
+nome = str(input('Nome do jogador: '))
+gols = str(input('Gols marcados: '))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(g=gols)
+else:
+    ficha(nome, gols)
