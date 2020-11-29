2992a21ad39c325715a24bfe77f34ecdba625c70
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-29 15:00:39 -0300

    Instructions inserted

---
+++ mundo-3/desafio104.py
@@ -0,0 +1,2 @@
+# Crie um programa que tenha a função leiaInt(), que vai funcionar semelhante à função input() do Python,
+# só que fazendo a validação para aceitar apenas um valor numérico. Exemplo: n = leiaInt('Digite um n: ')
