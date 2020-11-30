eb65e0e2c67d0e288620bb4de5f69a2ea1567409
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-30 13:13:19 -0300

    Function leiaInt() created

--- mundo-3/desafio104.py
+++ mundo-3/desafio104.py
@@ -1,2 +1,16 @@
 # Crie um programa que tenha a função leiaInt(), que vai funcionar semelhante à função input() do Python,
 # só que fazendo a validação para aceitar apenas um valor numérico. Exemplo: n = leiaInt('Digite um n: ')
+def leiaInt(mens):
+    mens = str(input('Digite um valor: '))
+    while True:
+        if mens.isnumeric():
+            mens = int(mens)
+            break
+        else:
+            print('Inválido! Digite um valor numérico.')
+            mens = str(input('Digite um valor: '))
+    print(f'{mens} é um número.')
+
+
+# Programa Principal
+mens = leiaInt('Digite um valor: ')
