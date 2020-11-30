a2c67b6def1dad4a731ca5d6b1bfd6c8c1f1ff17
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-11-30 14:10:28 -0300

    Instructions inserted

---
+++ mundo-3/desafio105.py
@@ -0,0 +1,7 @@
+# Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário
+# com as seguintes informações:
+# a) Quantidade de notas;
+# b) A maior nota;
+# c) A menor nota;
+# d) A média da turma;
+# e) A situação (opcional).
