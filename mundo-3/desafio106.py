0ab15f7dfcdd5ebed5bcd7123ae20bd594894825
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-12-04 15:38:03 -0300

    Instructions inserted

---
+++ mundo-3/desafio106.py
@@ -0,0 +1,2 @@
+# Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual
+# vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.
