67c6383b7a3fb77aca89f62bc4550cc959578c72
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-12-15 03:51:12 -0300

    First outputs

--- mundo-3/desafio107/desafio107.py
+++ mundo-3/desafio107/desafio107.py
@@ -1,2 +1,9 @@
 # Crie um módulo chamado moeda.py que tenham as funções incorporadas aumentar(), diminuir(),
 # dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
+import moeda
+num = int(input('Digite o valor da moeda: '))
+aum = moeda.aumentar(num)
+dim = moeda.diminuir(num)
+dob = moeda.dobro(num)
+met = moeda.metade(num)
+print(f'O valor da moeda é {num}\nQuando aumentada tem seu valor {aum}\nQuando diminuída tem valor {dim}\nSeu dobro é {dob}\nSua metade é {met}')
---
+++ mundo-3/desafio107/moeda.py
@@ -0,0 +1,15 @@
+def aumentar(n):
+    r = n + 1
+    return r
+
+def diminuir(n):
+    r = n - 1
+    return r
+
+def dobro(n):
+    r = n * 2
+    return r
+
+def metade(n):
+    r = n / 2
+    return r
