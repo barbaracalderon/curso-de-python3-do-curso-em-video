65ead6a4127b19e189d02017581ccebbda41d511
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-12-28 04:07:07 -0300

    Instructions inserted

---
+++ mundo-3/desafio113.py
@@ -0,0 +1,2 @@
+# Reescreva a função leiaInt() que fizemos no Desafio 104, incluindo agora a possibilidade da digitação
+# de um número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
