68c1da48f07699eb463e502d25ebc4f03414b26e
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2020-12-28 19:08:47 -0300

    Code to check if pudim website is available

--- mundo-3/desafio114.py
+++ mundo-3/desafio114.py
@@ -1,1 +1,9 @@
 # Crie um código em Python que teste se o site pudim está acessível pelo computador usado.
+import urllib
+import urllib.request
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except:
+    print('\033[31mO site do pudim não está disponível :(\033[m')
+else:
+    print('\033[32mO site do pudim está no ar! :)\033[m')
