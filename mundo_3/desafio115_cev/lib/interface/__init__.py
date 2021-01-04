aea01369ed223c7dd7079ea8ece3155b59ee9b51
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-01-03 22:34:05 -0300

    First part of Guanabara's solution

--- mundo_3/desafio115_cev/lib/interface/__init__.py
+++ mundo_3/desafio115_cev/lib/interface/__init__.py
@@ -1,7 +1,32 @@
-def linha(tam = 42):
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO! Digite um número válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[31mUsuário escolheu não digitar uma opção.\033[m')
+            return 0
+        else:
+            return n
+
+def linha(tam=40):
     return '-' * tam
+
 
 def cabecalho(txt):
     print(linha())
-    print(txt)
+    print(txt.center(40))
     print(linha())
+
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    cont = 1
+    for item in lista:
+        print(f'\033[33m{cont}\033[m: \033[30m{item}\033[m')
+        cont += 1
+    print(linha())
+    opcao = leiaInt('\033[33mDigite sua opção: \033[m')
+    return opcao
--- mundo_3/desafio115_cev/sistema.py
+++ mundo_3/desafio115_cev/sistema.py
@@ -1,2 +1,15 @@
 from mundo_3.desafio115_cev.lib.interface import *
-cabecalho('Testando 123')
+from time import sleep
+
+while True:
+    resposta = menu(['Cadastrar Pessoa', 'Ver Cadastro', 'Sair do Sistema'])
+    if resposta == 1:
+        print('CADASTRAR PESSOA')
+    elif resposta == 2:
+        print('VER CADASTRO')
+    elif resposta == 3:
+        print('<< SISTEMA ENCERRADO >>')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida do menu.\033[m')
+    sleep(1)
