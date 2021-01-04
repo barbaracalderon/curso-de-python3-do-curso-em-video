5af8a52088e6398b6248c6090b08c92aae00b16e
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-01-03 21:36:59 -0300

    Instructions Inserted

---
+++ mundo_3/desafio115_cev/lib/interface/__init__.py
@@ -0,0 +1,7 @@
+def linha(tam = 42):
+    return '-' * tam
+
+def cabecalho(txt):
+    print(linha())
+    print(txt)
+    print(linha())
---
+++ mundo_3/desafio115_cev/sistema.py
@@ -0,0 +1,2 @@
+from mundo_3.desafio115_cev.lib.interface import *
+cabecalho('Testando 123')
